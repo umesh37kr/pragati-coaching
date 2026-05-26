ed94eddd6aa9f321eaa714d4fea2a7f10a0430ed
umesh <umesh37kr@gmail.com>
2026-05-26 21:55:05 +0530

    header and footer component kept on right place

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Header />
-      <body className="min-h-full flex flex-col">{children}</body>
-      <Footer />
+      <body className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
