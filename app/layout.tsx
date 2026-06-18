d86219a2b97927613f9c1983633165f4d5f684e1
umesh <umesh37kr@gmail.com>
2026-06-18 21:54:24 +0530

    fix layout hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-screen flex flex-col">
-        <main className="flex-1">{children}</main>
+        {children}
       </body>
     </html>
   );
